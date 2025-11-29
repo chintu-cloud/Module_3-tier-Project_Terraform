93681d0651efa50c53e98bd39eb2a7a5e50da7d8
CHANDAN MOHANTY <github.chintu02@gmail.com>
2025-11-29 17:43:55 +0530

    Create variable.tf

---
+++ ROOT-MODULE/LAUNCH-TEMPLATE/variable.tf
@@ -0,0 +1,7 @@
+variable "project_name" {}
+variable "frontend_ami" {}
+variable "backend_ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "frontend_sg_id" {}
+variable "backend_sg_id" {}
